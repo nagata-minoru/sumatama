a441151fbad514c5f2ff21a21a200372694e05d0
NAGATA Minoru <nagata@lean-system.co.jp>
2014-12-10 10:18:35 +0900

    エラー処理

--- fb/post_feed.php
+++ fb/post_feed.php
@@ -6,6 +6,11 @@ require_once('lib/Feed.php');
 
 lib\FacebookUtil::fbInit();
 
-$user = new lib\User($_POST['user_id'], $_POST['access_token']);
-$feed = new lib\Feed($user);
-$feed->post($_POST['message']);
+try {
+    $user = new lib\User($_POST['user_id'], $_POST['access_token']);
+    $feed = new lib\Feed($user);
+    $feed->post($_POST['message']);
+    echo 'success';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
