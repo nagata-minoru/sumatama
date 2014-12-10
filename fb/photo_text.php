549355367c9ed7031274c74644500c1af18fa903
NAGATA Minoru <nagata@lean-system.co.jp>
2014-12-10 10:17:29 +0900

    合成文字をテレビの字幕っぽくする

--- fb/photo_text.php
+++ fb/photo_text.php
@@ -14,11 +14,27 @@ function getImage($url, $temp) {
 
 header('Content-type: image/jpeg');
 $temp = '/tmp/temp.jpg';
+unlink($temp);
 getImage($_GET['url'], $temp);
 $image = new Imagick($temp);
 
+$page = $image->getImagePage();
+$fontSize = max($page['width'], $page['height']) / 10;
 $draw = new ImagickDraw();
-$draw->annotation(20, 50, $_GET['txt']);
+$draw->setFontSize($fontSize);
+
+$text = $_GET['txt'];
+$metrics = $image->queryFontMetrics($draw, $text);
+
+$draw->setFillColor('#fff');
+$draw->setFillOpacity(0.5);
+$draw->rectangle(20, 50 - $fontSize, 20 + $metrics['textWidth'], 50);
+
+$draw->setFillColor('#00f');
+$draw->setFillOpacity(1);
+$draw->annotation(20, 50, $text);
+
 $image->drawImage($draw);
 
 echo $image;
+$image->clear();
